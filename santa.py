a301d361e04c6be17168d7125b26bdfc064b1acc
Selena Shang <selena@berkeley.edu>
2015-11-13 15:37:17 -0800

    Added functionality to save assignments
    Don't forget who you have!

--- santa.py
+++ santa.py
@@ -2,6 +2,7 @@ import random
 import os
 import sys
 
+
 def assign(players, gifts):
     """
     Assigns each player to a Secret Santa.
@@ -12,7 +13,13 @@ def assign(players, gifts):
         assignee = random.choice([p for p in players if p != player and p not in already_assigned])
         secret_santas[player] = assignee
         already_assigned.append(assignee)
+
+    # Write to file so that you don't forget who is assigned to who
+    with open('secret_santas.txt', 'w') as f:
+        f.writelines('{}: {}\n'.format(k,v) for k, v in secret_santas.items())
     return secret_santas
+
+# Begin main function    
 
 players = [player.split(":",1)[0].lower() for player in open("players.txt")]
 gifts = [player.split(":",1)[1].strip().lower() for player in open("players.txt")]
@@ -30,9 +37,10 @@ try:
             player = raw_input("You misspelled your name lol. Try again.\n").lower()
 
         print("You're assigned to " + assignments[player].capitalize() + " and he/she/xhe wants " + participant_dictionary[assignments[player]] +".")
-        if raw_input("\nPress any key to clear screen or q to quit\n") == 'q':
+        if raw_input("\nPress enter to clear screen for the next person or q to quit.\n") == 'q':
             print "Have fun!"
             exit()
         os.system("clear")
 except IndexError:
     pass
+
