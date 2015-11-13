3742b9ae9d9d72f9e665475308510bf88c0bdd58
Selena Shang <selena@berkeley.edu>
2015-11-13 13:59:56 -0800

    Added gift options

--- santa.py
+++ santa.py
@@ -1,7 +1,7 @@
 import random
 import os
 
-def assign(players):
+def assign(players, gifts):
     """
     Assigns each player to a Secret Santa.
     """
@@ -13,16 +13,24 @@ def assign(players):
         already_assigned.append(assignee)
     return secret_santas
 
+players = [player.split(":",1)[0].lower() for player in open("players.txt")]
+gifts = [player.split(":",1)[1].strip().lower() for player in open("players.txt")]
+participant_dictionary = {}
 
-players = [player.strip().lower() for player in open("players.txt")]
-assignments = assign(players)
+i=0
+for player in players:
+    participant_dictionary[player] = gifts[i]
+    i+=1
+
+assignments = assign(players, gifts)
 while True:
     player = raw_input("What's your name?\n").lower()
     if player == "q":
         exit()
-    print("You're assigned to " + assignments[player].capitalize() + ".")
+    print("You're assigned to " + assignments[player].capitalize() + " and she wants " + participant_dictionary[assignments[player]] +".")
     if raw_input("\nPress enter to clear screen or 'q' to quit\n") == 'q':
         exit()
     os.system("clear")
+
 
 
